ca62b675e5c2215f3792ea7504b6a650d832ac2b
Hack Reactor Students <github@hackreactor.com>
2013-04-29 14:54:35 -0700

    Finished queue functional-shared

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,1 +1,33 @@
-// Hey! Copy your code from src/functional/queue.js and paste it here
+var makeQueue = function(){
+  // Use an object with numeric keys to store values
+  // Implement the methods below
+  var queue = {};
+  queue.storage = {};
+  queue.storage.size = 0; // Hint: set an initial value here
+  queue.front = 0;
+  _.extend(queue, queueMethods);
+  return queue;
+};
+
+
+var queueMethods = {};
+
+queueMethods.add = function(value){
+  this.storage[this.size()+this.front] = value;
+  this.storage.size+=1;
+};
+
+queueMethods.remove = function(){
+  if (this.size()===0) {
+    throw new Error('Can\'t remove from empty queue');
+  }
+  var obj = this.storage[this.front];
+  delete this.storage[this.front];
+  this.storage.size-=1;
+  this.front+=1;
+  return obj;
+};
+
+queueMethods.size = function(){
+  return this.storage.size;
+};
