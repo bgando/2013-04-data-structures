7488e0d4b79a5a6959bad50811ded57cae075f9e
Hack Reactor Students <github@hackreactor.com>
2013-04-29 17:14:42 -0700

    Completed pseudoclassical versions

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,1 +1,28 @@
-// Hey! Copy your code from src/prototypal/queue.js and paste it here
+//profiled:4051ms
+var Queue = function(){
+  // Use an object with numeric keys to store values
+  // Implement the methods below
+  this.storage = {};
+  this.storage.size = 0; // Hint: set an initial value here
+  this.front = 0;
+};
+
+Queue.prototype.add = function(value){
+  this.storage[this.size()+this.front] = value;
+  this.storage.size+=1;
+};
+
+Queue.prototype.remove = function(){
+  if (this.size()===0) {
+    throw new Error('Can\'t remove from empty queue');
+  }
+  var obj = this.storage[this.front];
+  delete this.storage[this.front];
+  this.storage.size-=1;
+  this.front+=1;
+  return obj;
+};
+
+Queue.prototype.size = function(){
+  return this.storage.size;
+};
--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,1 +1,26 @@
-// Hey! Copy your code from src/prototypal/stack.js and paste it here
+//17463
+var Stack = function(){
+  // Use an object with numeric keys to store values
+  // Implement the methods below
+  this.length = 0;
+  this.storage = {};
+};
+
+Stack.prototype.add = function(value){
+  this.storage[this.length] = value;
+  this.length+=1;
+
+};
+Stack.prototype.remove = function(){
+  if (this.length===0) {
+    throw new Error('cannot remove from empty stack');
+  }
+  var obj = this.storage[this.length-1];
+  delete this.storage[this.length-1];
+  this.length-=1;
+  return obj;
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
